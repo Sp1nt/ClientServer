c25815481a453233596d9894bc1c0e2df9272d34
Spr1nt <thespr1nt0ff24@gmail.com>
2024-03-21 09:41:10 +0200

    new

--- 5 lesson_18_03/hw5/HttpExample/HttpGetHtmlPAge/HttpGetHtmlPAge.cpp
+++ 5 lesson_18_03/hw5/HttpExample/HttpGetHtmlPAge/HttpGetHtmlPAge.cpp
@@ -1,46 +1,10 @@
 ﻿#pragma comment (lib, "Ws2_32.lib")
-#define _CRT_SECURE_NO_WARNINGS
 #include <Winsock2.h>
 #include <ws2tcpip.h>
 
 #include <iostream>
 #include <string>
-#include <ctime>
 using namespace std;
-
-
-string findKey(const string& jsonString, const string& fieldName) {
-
-    string pathMarker = "\"" + fieldName + "\":";
-
-
-    int  startInd = jsonString.find(pathMarker);
-    if (startInd != string::npos) {
-
-        int  sPos = jsonString.find_first_not_of(" \t\r\n", startInd + pathMarker.length());
-        if (sPos != string::npos) {
-
-            int  ePos = jsonString.find_first_of(",}", sPos);
-            if (ePos != string::npos) {
-
-                return jsonString.substr(sPos, ePos - sPos);
-            }
-        }
-    }
-
-    return "";
-}
-
-string convertNumToTime(const string& timestamp) {
-    time_t time = stoi(timestamp);
-
-    struct tm* timeInfo = gmtime(&time);
-
-    char temp[105];
-    strftime(temp, 105, "%H:%M:%S", timeInfo);
-
-    return string(temp);
-}
 
 int main()
 {
@@ -54,14 +18,14 @@ int main()
 
         cout << "WSAStartup failed with error: " << err << endl;
         return 1;
-    }  
+    }
 
     //инициализация структуры, для указания ip адреса и порта сервера с которым мы хотим соединиться
-   
-    char hostname[255] = "api.openweathermap.org";
-    
-    addrinfo* result = NULL;    
-    
+
+    char hostname[255] = "nz.ua";
+
+    addrinfo* result = NULL;
+
     addrinfo hints;
     ZeroMemory(&hints, sizeof(hints));
     hints.ai_family = AF_INET;
@@ -73,7 +37,8 @@ int main()
         cout << "getaddrinfo failed with error: " << iResult << endl;
         WSACleanup();
         return 3;
-    }     
+    }
+
 
     SOCKET connectSocket = INVALID_SOCKET;
     addrinfo* ptr = NULL;
@@ -89,7 +54,7 @@ int main()
             return 1;
         }
 
-       //3. Соединяемся с сервером
+        //3. Соединяемся с сервером
         iResult = connect(connectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
         if (iResult == SOCKET_ERROR) {
             closesocket(connectSocket);
@@ -101,13 +66,13 @@ int main()
 
     //4. HTTP Request
 
-    string uri = "/data/2.5/weather?q=Odessa&appid=75f6e64d49db78658d09cb5ab201e483&mode=JSON";
+    string uri = "/";
 
-    string request = "GET " + uri + " HTTP/1.1\n"; 
+    string request = "GET " + uri + " HTTP/1.1\n";
     request += "Host: " + string(hostname) + "\n";
     request += "Accept: */*\n";
-    request += "Accept-Encoding: gzip, deflate, br\n";   
-    request += "Connection: close\n";   
+    request += "Accept-Encoding: gzip, deflate, br\n";
+    request += "Connection: close\n";
     request += "\n";
 
     //отправка сообщения
@@ -131,7 +96,7 @@ int main()
     do {
         respLength = recv(connectSocket, resBuf, BUFFERSIZE, 0);
         if (respLength > 0) {
-            response += string(resBuf).substr(0, respLength);           
+            response += string(resBuf).substr(0, respLength);
         }
         else {
             cout << "recv failed: " << WSAGetLastError() << endl;
@@ -142,26 +107,7 @@ int main()
 
     } while (respLength == BUFFERSIZE);
 
-    string country = findKey(response, "country");
-    string city = findKey(response, "name");
-    string date = findKey(response, "dt");
-    date = convertNumToTime(date);
-    string lon = findKey(response, "lon");
-    string lat = findKey(response, "lat");
-    string temperature = findKey(response, "temp");
-    string sunrise = findKey(response, "sunrise");
-    sunrise = convertNumToTime(sunrise);
-    string sunset = findKey(response, "sunset");
-    sunset = convertNumToTime(sunset);
-
-    cout << "Country: " << country << endl;
-    cout << "City: " << city << endl;
-    cout << "Date: " << date << endl;
-    cout << "CoordinatesX: " << lon << endl;
-    cout << "CoordinatesY: " << lat << endl;
-    cout << "Temperature: " << temperature << endl;
-    cout << "Sunrise time: " << sunrise << endl;
-    cout << "Sunset time: " << sunset << endl;
+    cout << response << endl;
 
     //отключает отправку и получение сообщений сокетом
     iResult = shutdown(connectSocket, SD_BOTH);
